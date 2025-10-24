01e9f4264cc82dfcee3bc7544c33d29ad3627c18
Eliomar Rodriguez <rodriguezeliomar088@gmail.com>
2025-10-25 00:57:30 +0200

    Update CodeMirror editor and build configuration
    Modified editor.bundle.js, lang-rv32i/index.js, and rollup.config.js to update editor logic, language support, and build settings. These changes improve document handling, language features, and build process for CodeMirror.

--- Sources/CodeMirror/src/lang-rv32i/index.js
+++ Sources/CodeMirror/src/lang-rv32i/index.js
@@ -5,17 +5,28 @@
 //  Created by Eliomar Alejandro Rodriguez Ferrer on 25/10/25.
 //
 
-// src/lang-rv32i/index.js
 import { LRLanguage, LanguageSupport } from "@codemirror/language";
 import { parser } from "./riscv.grammar.js";
+import { HighlightStyle, tags } from "@codemirror/highlight";
 
+// definisci il linguaggio
 export const rv32iLanguage = LRLanguage.define({
   parser: parser,
   languageData: {
 	commentTokens: { line: "//" },
-  },
+  }
 });
 
+// definisci colori dei token
+export const rv32iHighlight = HighlightStyle.define([
+  { tag: tags.keyword, color: "#ff9f1c" },      // istruzioni e direttive
+  { tag: tags.variableName, color: "#2ec4b6" }, // registri
+  { tag: tags.number, color: "#8ac926" },       // numeri
+  { tag: tags.labelName, color: "#e71d36" },    // label
+  { tag: tags.string, color: "#9d4edd" },       // stringhe
+]);
+
+// funzione da importare
 export function rv32i() {
-  return new LanguageSupport(rv32iLanguage);
+  return new LanguageSupport(rv32iLanguage, [rv32iHighlight]);
 }
--- Sources/CodeMirror/src/rollup.config.js
+++ Sources/CodeMirror/src/rollup.config.js
@@ -4,7 +4,7 @@ export default {
   input: "./editor.js",
   output: {
     file: "./build/editor.bundle.js",
-    format: "umd",
+    format: "es",
     extend: true,
     name: "CodeMirror",
     exports: "named",
